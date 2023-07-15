e038f7e705c036882a078962b0a2e4c7fefe02ee
sikha_puthanveedu <69745070+sikhamohan@users.noreply.github.com>
2023-07-15 00:53:11 -0400

    Add files via upload
    Added file

---
+++ parsingbookmarkjson.py
@@ -0,0 +1,23 @@
+import json
+
+def parse_bookmark_node(node, urls):
+    if "children" in node:
+        for child in node["children"]:
+            if "url" in child:
+                urls.append(child["url"])
+            else:
+                parse_bookmark_node(child, urls)
+
+def parse_chrome_bookmarks(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        urls = []
+        for root in data["roots"].values():
+            parse_bookmark_node(root, urls)
+        return urls
+
+# Example usage:
+file_path = r"C:\Users\sikha\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+bookmark_urls = parse_chrome_bookmarks(file_path)
+for url in bookmark_urls:
+    print(url)
